090b9b5afd1174fb8ceedb0a48c06e819f3c273b
davegregg@gmail.com <davegregg@gmail.com>
2023-04-28 15:21:48 -0400

    Add starter files. Add basic addition calculator.

---
+++ main.js
@@ -0,0 +1,16 @@
+// The "document" object is the DOM (Document Object Model)
+// The "DOM" is another way of calling the "HTML tree structure"
+let calculatorForm = document.getElementById('calculator-form')
+let inputX = document.getElementById('number-x')
+let inputY = document.getElementById('number-y')
+let resultElement = document.getElementById('result')
+
+function onCalculate (event) {
+    event.preventDefault()
+
+    let sum = Number(inputX.value) + Number(inputY.value)
+    resultElement.innerHTML = "<h4>" + sum + "</h4>"
+}
+
+calculatorForm.onsubmit = onCalculate
+
